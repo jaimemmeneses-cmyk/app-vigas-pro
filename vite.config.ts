f081d133fa5e770d42dbc4548d339c4a20ab77b5
Jaime Meneses <jaime.m.meneses@gmail.com>
2025-12-02 23:29:27 -0500

    Arreglando pantalla gris en Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app-vigas-pro/',  // <--- IMPORTANTE: Debe ser el nombre exacto de tu repo entre barras
+  base: '/',  // <--- CAMBIO REALIZADO: Ahora apunta a la raíz
 })
